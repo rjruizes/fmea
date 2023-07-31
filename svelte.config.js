a0d06da1074c31c4ed4540e5c96c30cd8420f371
Roman Ruiz-Esparza <2654709+rjruizes@users.noreply.github.com>
2023-07-31 18:57:31 -0400

    disable prerendering for auth

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,11 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({ runtime: 'edge' })
+		adapter: adapter({ runtime: 'edge' }),
+		prerender: {
+			crawl: false,
+			entries: []
+		}
 	}
 };
 
